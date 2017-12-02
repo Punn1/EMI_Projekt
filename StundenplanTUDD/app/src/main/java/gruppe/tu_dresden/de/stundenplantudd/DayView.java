95346ae67f6d1c31fa87454efa30c65429d36236
tyhxy1123 <tyhxy123@gmail.com>
2017-12-02 19:54:57 +0100

    GitSyncTest

---
+++ StundenplanTUDD/app/src/main/java/gruppe/tu_dresden/de/stundenplantudd/DayView.java
@@ -0,0 +1,13 @@
+package gruppe.tu_dresden.de.stundenplantudd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_view);
+    }
+}
